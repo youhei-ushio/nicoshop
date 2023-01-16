331b99aec48bd954113d15a197dff9b7e711aac1
youhei-ushio <youhei@sea-salt.jp>
2023-01-17 04:00:28 +0900

    add comment

--- app/Contexts/Sales/Domain/Event/OrderNotYetAccepted.php
+++ app/Contexts/Sales/Domain/Event/OrderNotYetAccepted.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\Contexts\Sales\Domain\Event;
 
+use App\Contexts\Sales\Domain\Entity\Order;
 use DateTimeImmutable;
 
 final class OrderNotYetAccepted
 {
+    /**
+     * @param Order\Item[] $items
+     */
     public function __construct(
         public readonly string $id,
         public readonly DateTimeImmutable $date,
